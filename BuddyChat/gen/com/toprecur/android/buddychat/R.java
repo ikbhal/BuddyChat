82c64d3f9ef74f58e6883c7e4243f01fe507af86
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2014-03-26 12:57:40 +0530

    adding class other files, will remove later.

--- BuddyChat/gen/com/toprecur/android/buddychat/R.java
+++ BuddyChat/gen/com/toprecur/android/buddychat/R.java
@@ -25,23 +25,22 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_contacts=0x7f08000e;
-        public static final int action_logout=0x7f08000f;
-        public static final int action_settings=0x7f080010;
-        public static final int chat_input=0x7f080001;
-        public static final int chat_list=0x7f080003;
-        public static final int error_messages=0x7f080004;
-        public static final int fragmentContainer=0x7f080000;
-        public static final int login_password=0x7f080006;
-        public static final int login_username=0x7f080005;
-        public static final int registerButton=0x7f080008;
-        public static final int register_password=0x7f08000a;
-        public static final int register_username=0x7f080009;
-        public static final int send_button=0x7f080002;
-        public static final int signInButton=0x7f08000c;
-        public static final int sign_in=0x7f080007;
-        public static final int sign_up=0x7f08000b;
-        public static final int task_description=0x7f08000d;
+        public static final int action_contacts=0x7f08000d;
+        public static final int action_logout=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
+        public static final int chat_input=0x7f080000;
+        public static final int chat_list=0x7f080002;
+        public static final int error_messages=0x7f080003;
+        public static final int login_password=0x7f080005;
+        public static final int login_username=0x7f080004;
+        public static final int registerButton=0x7f080007;
+        public static final int register_password=0x7f080009;
+        public static final int register_username=0x7f080008;
+        public static final int send_button=0x7f080001;
+        public static final int signInButton=0x7f08000b;
+        public static final int sign_in=0x7f080006;
+        public static final int sign_up=0x7f08000a;
+        public static final int task_description=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_chat=0x7f030000;
@@ -60,8 +59,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
-        public static final int title_activity_chat=0x7f050004;
+        public static final int title_activity_chat=0x7f050005;
         public static final int title_activity_login=0x7f050003;
+        public static final int title_activity_register=0x7f050004;
     }
     public static final class style {
         /** 
